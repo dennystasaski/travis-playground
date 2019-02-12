81b1e5e99d34c964d6aecf7b61690f4b369f26d1
Denny Stasaski <dstasaski@gmail.com>
2019-02-11 19:57:28 -0600

    added deploy script tasks

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rsync -r -e 'ssh -p 9250' --delete-after --quiet $TRAVIS_BUILD_DIR/app shelby@45.27.203.103:/home/shelby/app/tmp
-#cp tmp/app/app.sh .
-#./app.sh
+ssh -p 9250 shelby@45.27.203.103
+cp tmp/app/app.sh .
+./app.sh
