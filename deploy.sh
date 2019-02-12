4fbdcfff3e1d888733bb4bf980b8c04762eaf5bf
Denny Stasaski <dstasaski@gmail.com>
2019-02-11 20:11:24 -0600

    fixed file paths

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rsync -r -e 'ssh -p 9250' --delete-after --quiet $TRAVIS_BUILD_DIR/app shelby@45.27.203.103:/home/shelby/app/tmp
-ssh -p 9250 shelby@45.27.203.103 cd app; cp tmp/app/app.sh .; ./app.sh
+ssh -p 9250 shelby@45.27.203.103 cp /home/shelby/app/tmp/app/app.sh /home/shelby/app; ./home/shelby/app.sh
 
